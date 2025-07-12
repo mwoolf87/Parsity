5b405bc250c2841a7b70db6e9ac68225134ef9e7
mwoolf87 <mwoolfdev@gmail.com>
2025-07-12 19:39:27 -0400

    first push on this repo

---
+++ ModernJS/app.js
@@ -0,0 +1,20 @@
+if (1 === 2) {
+  console.log("its true!!!");
+} else {
+  console.log("its false");
+}
+
+let num = 38;
+if (num % 2 !== 0) {
+  console.log("odd number");
+}
+
+let nums = 11;
+
+if (nums >= 1 && nums <= 10) {
+  console.log("number is between 1 and 10");
+} else {
+  console.log("number is out of range");
+}
+
+console.log(!true);
