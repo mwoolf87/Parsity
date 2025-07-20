6113eea718c598ed10cf570816f488d8396de4b8
mwoolf87 <mwoolfdev@gmail.com>
2025-07-20 19:58:19 -0400

    object methods contd

--- ModernJS/app.js
+++ ModernJS/app.js
@@ -937,76 +937,76 @@ const runner = {
 //   },
 // };
 
-const annoyer = {
-  phrases: [
-    "literally",
-    "cray cray",
-    "I can't even",
-    "Totes",
-    "YOLO",
-    "Can't Stop, Won't Stop",
-  ],
-  pickPhrase() {
-    const { phrases } = this;
-    const idx = Math.floor(Math.random() * phrases.length);
-    return phrases[idx];
-  },
-  start() {
-    this.timerId = setInterval(() => {
-      console.log(this.pickPhrase());
-    }, 3000);
-  },
-  stop() {
-    clearInterval(this.timerId);
-  },
-};
+// const annoyer = {
+//   phrases: [
+//     "literally",
+//     "cray cray",
+//     "I can't even",
+//     "Totes",
+//     "YOLO",
+//     "Can't Stop, Won't Stop",
+//   ],
+//   pickPhrase() {
+//     const { phrases } = this;
+//     const idx = Math.floor(Math.random() * phrases.length);
+//     return phrases[idx];
+//   },
+//   start() {
+//     this.timerId = setInterval(() => {
+//       console.log(this.pickPhrase());
+//     }, 3000);
+//   },
+//   stop() {
+//     clearInterval(this.timerId);
+//   },
+// };
 
-const makeDeck = () => {
-  return {
-    deck: [],
-    drawnCards: [],
-    suits: ["hearts", "diamonds", "spades", "clubs"],
-    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
-    initializeDeck() {
-      const { suits, values, deck } = this;
-      for (let value of values.split(",")) {
-        for (let suit of suits) {
-          deck.push({ value, suit });
-        }
-      }
-      // return deck;
-    },
-    drawCard() {
-      const card = this.deck.pop();
-      this.drawnCards.push(card);
-      return card;
-    },
-    drawMultiple(numCards) {
-      const cards = [];
-      for (let i = 0; i < numCards; i++) {
-        cards.push(this.drawCard());
-      }
-      return cards;
-    },
-    shuffle() {
-      const { deck } = this;
-      //loop over array backwards
-      for (let i = deck.length - 1; i > 0; i--) {
-        //pick random index before current element
-        let j = Math.floor(Math.random() * (i + 1));
-        //swap
-        [deck[i], deck[j]] = [deck[j], deck[i]];
-      }
-    },
-  };
-};
+// const makeDeck = () => {
+//   return {
+//     deck: [],
+//     drawnCards: [],
+//     suits: ["hearts", "diamonds", "spades", "clubs"],
+//     values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+//     initializeDeck() {
+//       const { suits, values, deck } = this;
+//       for (let value of values.split(",")) {
+//         for (let suit of suits) {
+//           deck.push({ value, suit });
+//         }
+//       }
+//       // return deck;
+//     },
+//     drawCard() {
+//       const card = this.deck.pop();
+//       this.drawnCards.push(card);
+//       return card;
+//     },
+//     drawMultiple(numCards) {
+//       const cards = [];
+//       for (let i = 0; i < numCards; i++) {
+//         cards.push(this.drawCard());
+//       }
+//       return cards;
+//     },
+//     shuffle() {
+//       const { deck } = this;
+//       //loop over array backwards
+//       for (let i = deck.length - 1; i > 0; i--) {
+//         //pick random index before current element
+//         let j = Math.floor(Math.random() * (i + 1));
+//         //swap
+//         [deck[i], deck[j]] = [deck[j], deck[i]];
+//       }
+//     },
+//   };
+// };
 
-const myDeck = makeDeck();
-myDeck.initializeDeck();
-myDeck.shuffle();
-const hand1 = myDeck.drawMultiple(2);
-const hand2 = myDeck.drawMultiple(2);
-const hand3 = myDeck.drawMultiple(5);
+// const myDeck = makeDeck();
+// myDeck.initializeDeck();
+// myDeck.shuffle();
+// const hand1 = myDeck.drawMultiple(2);
+// const hand2 = myDeck.drawMultiple(2);
+// const hand3 = myDeck.drawMultiple(5);
 
-const deck2 = makeDeck();
-deck2.initializeDeck();
+// const deck2 = makeDeck();
+// deck2.initializeDeck();
