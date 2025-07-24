81c4b637f51cf3d5b3b9461058430dd3879dba6c
mwoolf87 <mwoolfdev@gmail.com>
2025-07-23 20:29:40 -0400

    creating, removing, and adding elements

--- ModernJS/DomManipulation/app.js
+++ ModernJS/DomManipulation/app.js
@@ -1,6 +1,16 @@
 const allLis = document.querySelectorAll("li");
 const h1 = document.querySelector("h1");
 const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+const todo = document.querySelector("#todos .todo");
+const parentUL = document.querySelector("ul");
+const newLi = document.createElement("li");
+newLi.innerText = "I AM A NEW LI";
+const firstLi = document.querySelector("li.todo");
+const ul = document.querySelector("section ul");
+const removeMe = ul.querySelector(".special");
+parentUL.insertBefore(newLi, firstLi);
+h1.remove();
+ul.removeChild(removeMe);
 // for (let i = 0; i < allLis.length; i++) {
 //   allLis[i].innerText = "WE ARE THE CHAMPIONS";
 // }
@@ -10,7 +20,7 @@ const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 // }
 
 // inline style is most specifc class style
-allLis.forEach((li, i) => {
-  const color = colors[i];
-  li.style.color = color;
-});
+// allLis.forEach((li, i) => {
+//   const color = colors[i];
+//   li.style.color = color;
+// });
