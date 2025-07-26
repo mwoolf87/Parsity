bb9e17681ab7ca0d8152610896ba0ea3bb4a39bb
mwoolf87 <mwoolfdev@gmail.com>
2025-07-26 17:05:43 -0400

    fun with forms

--- ModernJS/addEventListeners/app.js
+++ ModernJS/addEventListeners/app.js
@@ -62,17 +62,53 @@
 //   console.log("KEY UP!");
 // });
 
-const addItemInput = document.querySelector("#addItem");
-const itemsUL = document.querySelector("#items");
+// const addItemInput = document.querySelector("#addItem");
+// const itemsUL = document.querySelector("#items");
 
-addItemInput.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    if (!this.value) return;
-    //add new item to list
-    const newItemText = this.value;
-    const newItem = document.createElement("li");
-    newItem.innerText = newItemText;
-    itemsUL.appendChild(newItem);
-    this.value = "";
-  }
+// addItemInput.addEventListener("keypress", function (e) {
+//   if (e.key === "Enter") {
+//     if (!this.value) return;
+//     //add new item to list
+//     const newItemText = this.value;
+//     const newItem = document.createElement("li");
+//     newItem.innerText = newItemText;
+//     itemsUL.appendChild(newItem);
+//     this.value = "";
+//   }
+// });
+
+const creditCardInput = document.querySelector("#cc");
+const termsCheckbox = document.querySelector("#terms");
+const veggieSelect = document.querySelector("#veggie");
+
+const form = document.querySelector("#signup-form");
+
+form.addEventListener("submit", function (e) {
+  alert("SUBMITTED THE FORM!");
+  console.log("cc", creditCardInput.value);
+  console.log("terms", termsCheckbox.checked);
+  console.log("veggieSelect", veggieSelect.value);
+  e.preventDefault();
 });
+
+const formData = {};
+for (let input of [creditCardInput, termsCheckbox, veggieSelect]) {
+  input.addEventListener("change", ({ target }) => {
+    const { name, type, value, checked } = target;
+    formData[name] = type === "checkbox" ? checked : value;
+    console.log(formData);
+  });
+}
+
+// creditCardInput.addEventListener("input", e => {
+//   console.log("Credit Card Changed", e);
+//   formData["cc"] = e.target.value;
+// });
+// veggieSelect.addEventListener("input", e => {
+//   console.log("veggie select Changed", e);
+//   formData["veggie"] = e.target.value;
+// });
+// termsCheckbox.addEventListener("input", e => {
+//   console.log("Terms Changed", e);
+//   formData["isChecked"] = e.target.checked;
+// });
