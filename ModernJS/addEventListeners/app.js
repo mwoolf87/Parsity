15b7b449a1baf1d35a018ac44ff93cc8a9f39dd4
mwoolf87 <mwoolfdev@gmail.com>
2025-07-26 13:33:13 -0400

    added coin game starter code and more practice with addEventListners

--- ModernJS/addEventListeners/app.js
+++ ModernJS/addEventListeners/app.js
@@ -27,23 +27,52 @@
 //   btn.style.top = `${height}px`;
 // });
 
-const colors = [
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "purple",
-  "indigo",
-  "violet",
-];
-const container = document.querySelector("#boxes");
-for (let color of colors) {
-  const box = document.createElement("div");
-  box.style.backgroundColor = color;
-  box.classList.add("box");
-  container.appendChild(box);
-  box.addEventListener("click", function () {
-    console.log(box.style.backgroundColor);
-  });
-}
+// const colors = [
+//   "red",
+//   "orange",
+//   "yellow",
+//   "green",
+//   "blue",
+//   "purple",
+//   "indigo",
+//   "violet",
+// ];
+// const changeColor = function () {
+//   const h1 = document.querySelector("h1");
+//   h1.style.color = this.style.backgroundColor;
+// };
+// const container = document.querySelector("#boxes");
+// for (let color of colors) {
+//   const box = document.createElement("div");
+//   box.style.backgroundColor = color;
+//   box.classList.add("box");
+//   container.appendChild(box);
+//   box.addEventListener("click", changeColor);
+// }
+
+// document.body.addEventListener("keypress", function (e) {
+//   console.log(e);
+// });
+// const input = document.querySelector("#username");
+
+// input.addEventListener("keydown", function () {
+//   console.log("KEY DOWN");
+// });
+// input.addEventListener("keyup", function () {
+//   console.log("KEY UP!");
+// });
+
+const addItemInput = document.querySelector("#addItem");
+const itemsUL = document.querySelector("#items");
+
+addItemInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    if (!this.value) return;
+    //add new item to list
+    const newItemText = this.value;
+    const newItem = document.createElement("li");
+    newItem.innerText = newItemText;
+    itemsUL.appendChild(newItem);
+    this.value = "";
+  }
+});
