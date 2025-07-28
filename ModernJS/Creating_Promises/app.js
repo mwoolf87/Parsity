4f19fbb25970ec28342f8cabf75d199309d9adb6
mwoolf87 <mwoolfdev@gmail.com>
2025-07-27 20:43:27 -0400

    more practicing promises

--- ModernJS/Creating_Promises/app.js
+++ ModernJS/Creating_Promises/app.js
@@ -41,6 +41,23 @@ const fakeRequest = url => {
           { id: 1, username: "Bilbo" },
           { id: 5, username: "Esmerelda" },
         ],
+        "/users/1": {
+          id: 1,
+          username: "Bilbo",
+          upvotes: 360,
+          city: "Lisbon",
+          topPostId: 454321,
+        },
+        "/users/5": {
+          id: 5,
+          username: "Esmerelda",
+          upvotes: 571,
+          city: "Honolulu",
+        },
+        "/posts/454321": {
+          id: 454321,
+          title: "Ladies & Gentlemen, may I introduce my pet pig, Hamlet",
+        },
         "/about": "This is the about page!",
       };
       const data = pages[url];
@@ -49,17 +66,33 @@ const fakeRequest = url => {
       } else {
         reject({ status: 404 });
       }
-    }, 2000);
+    }, 1000);
   });
 };
 
 fakeRequest("/users")
   .then(res => {
-    console.log("Status code", res.status);
-    console.log("Status code", res.data);
-    console.log("REQUEST WORKED!");
+    console.log(res);
+    const id = res.data[0].id;
+    return fakeRequest(`/users/${id}`);
   })
-  .catch(res => {
-    console.log(res.status);
-    console.log("REQUEST FAILED");
+  .then(res => {
+    console.log(res);
+    const postId = res.data.topPostId;
+    return fakeRequest(`/posts/${postId}`);
+  })
+  .then(res => console.log(res))
+  .catch(error => {
+    console.log("OH NO!, ERROR!!!", error);
   });
+
+// fakeRequest("/users")
+//   .then(res => {
+//     console.log("Status code", res.status);
+//     console.log("Status code", res.data);
+//     console.log("REQUEST WORKED!");
+//   })
+//   .catch(res => {
+//     console.log(res.status);
+//     console.log("REQUEST FAILED");
+//   });
