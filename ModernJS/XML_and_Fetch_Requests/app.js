b892d6b4cf6d394da730457a44bd079cd4161d90
mwoolf87 <mwoolfdev@gmail.com>
2025-07-29 21:23:04 -0400

    xml and fetch requests

---
+++ ModernJS/XML_and_Fetch_Requests/app.js
@@ -0,0 +1,31 @@
+// const firstRequest = new XMLHttpRequest();
+// firstRequest.addEventListener("load", function () {
+//   console.log("FIRST REQUEST WORKED");
+//   const data = JSON.parse(this.responseText);
+//   const filmURL = data[0].films[0];
+//   const filmReq = new XMLHttpRequest();
+//   filmReq.addEventListener("load", function () {
+//     console.log("SECOND REQUEST WORKED");
+//     const filmData = JSON.parse(this.responseText);
+//     console.log(filmData.title);
+//   });
+//   filmReq.addEventListener("error", function (e) {
+//     console.log("ERROR!", e);
+//   });
+//   filmReq.open("GET", filmURL);
+//   filmReq.send();
+//   //   for (let planet of data) {
+//   //     console.log(planet.name);
+//   //   }
+// });
+// firstRequest.addEventListener("error", () => {
+//   console.log("ERROR!!!!!");
+// });
+
+// firstRequest.open("GET", "https://swapi.info/api/planets");
+// firstRequest.send();
+// console.log("Request Sent!");
+
+fetch("https://swapi.info/api/planets").then(response => {
+  console.log(response);
+});
