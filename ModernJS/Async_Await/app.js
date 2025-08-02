b7e4050121cfa1d1decf4fb9266b22e4a728ea6a
mwoolf87 <mwoolfdev@gmail.com>
2025-08-02 15:07:01 -0400

    practicing async await with promise.all

--- ModernJS/Async_Await/app.js
+++ ModernJS/Async_Await/app.js
@@ -38,7 +38,57 @@
 //   console.log(res.data);
 // });
 
-async function getPlanets() {
-  const res = await xios.get("https://swapi.info/api/planets");
-  console.log(res.data);
+// async function getPlanets() {
+//   try {
+//     const res = await axios.get("https://swapi.info/api/planets");
+//     console.log(res.data);
+//   } catch (e) {
+//     console.log("IN CATCH!!!", e);
+//   }
+// }
+
+// getPlanets();
+
+// --- SEQUENTIAL REQUEST ---- //
+// async function get3Pokemon() {
+//   const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
+//   const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
+//   const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// get3Pokemon();
+
+// ---- PARALLELL REQUEST ---- //
+// async function get3Pokemon() {
+//   const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
+//   const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
+//   const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
+//   const poke1 = await prom1;
+//   const poke2 = await prom2;
+//   const poke3 = await prom3;
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// get3Pokemon();
+
+// ---- PROMISE.ALL EXAMPLE ---- //
+async function get3Pokemon() {
+  const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/1");
+  const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
+  const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
+  const results = await Promise.all([prom1, prom2, prom3]);
+  printPokemon(results);
 }
+
+function printPokemon(results) {
+  for (let pokemon of results) {
+    console.log(pokemon.data.name);
+  }
+}
+
+get3Pokemon();
