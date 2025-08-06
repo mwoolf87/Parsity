3acd6305f071d16eef8c0f77b4cab7258c5781ff
mwoolf87 <mwoolfdev@gmail.com>
2025-08-05 22:07:33 -0400

    refactored index.html and index.js to dynamically create html with javascript

--- ModernJS/movieFight/index.js
+++ ModernJS/movieFight/index.js
@@ -13,19 +13,36 @@ const fetchData = async searchTerm => {
 
   return response.data.Search;
 };
-
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search for a Movie</b></label>
+<input class="input"/>
+<div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+</div>
+`;
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
+
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+
   for (let movie of movies) {
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <img src="${movie.Poster}"/>
-    <h1>${movie.Title}</h1>
+    const option = document.createElement("a");
+    const imageSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+    <img src="${imageSrc}"/>
+    ${movie.Title}
     `;
 
-    document.querySelector("#target").appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 
