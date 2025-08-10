69e9270e31b521fcfb0d5a359ed010a6ab5f270e
mwoolf87 <mwoolfdev@gmail.com>
2025-08-10 12:01:58 -0400

    starter files for learning node

---
+++ ModernJS/node/index.js
@@ -0,0 +1,8 @@
+const counterObject = require("./myscript");
+
+console.log(counterObject.getCounter());
+counterObject.incrementCounter();
+console.log(counterObject.getCounter());
+
+const newCounterObject = require("./myscript");
+console.log(newCounterObject.getCounter());
