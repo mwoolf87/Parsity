e706905ccb4405d98adcb87dfc5278f9f611f5d9
mwoolf87 <mwoolfdev@gmail.com>
2025-08-14 22:16:21 -0400

    restarted moviefight

--- ModernJS/movieFight/index.js
+++ ModernJS/movieFight/index.js
@@ -1,1 +1,17 @@
+const fetchData = async searchTerm => {
+  const response = await axios.get("http://www.omdbapi.com", {
+    params: {
+      apikey: "39132f6b",
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
+};
 
+const input = document.querySelector("input");
+
+const onInput = e => {
+  fetchData(e.target.value);
+};
+
+input.addEventListener("input", debounce(onInput, 500));
