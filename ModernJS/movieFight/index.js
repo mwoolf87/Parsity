f7060b26ce05e59b63b87d2c04efdffca0a41772
mwoolf87 <mwoolfdev@gmail.com>
2025-08-14 22:34:53 -0400

    added logic to render movie.Poster and movie.Title to a div

--- ModernJS/movieFight/index.js
+++ ModernJS/movieFight/index.js
@@ -5,13 +5,26 @@ const fetchData = async searchTerm => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
 
-const onInput = e => {
-  fetchData(e.target.value);
+const onInput = async e => {
+  const movies = await fetchData(e.target.value);
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <img src="${movie.Poster}"/ > 
+      <h1>${movie.Title}</h1>
+    `;
+    document.querySelector("#target").appendChild(div);
+  }
 };
 
 input.addEventListener("input", debounce(onInput, 500));
