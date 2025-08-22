fc36b126d5db142e67123ad5e9f4696f7a15de37
mwoolf87 <mwoolfdev@gmail.com>
2025-08-21 22:05:20 -0400

    deleted old files

--- ModernJS/node/index.js
+++
@@ -1,8 +0,0 @@
-const counterObject = require("./myscript");
-
-console.log(counterObject.getCounter());
-counterObject.incrementCounter();
-console.log(counterObject.getCounter());
-
-const newCounterObject = require("./myscript");
-console.log(newCounterObject.getCounter());
