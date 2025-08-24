734a60825337e889f097a13392366da45d4f4474
mwoolf87 <mwoolfdev@gmail.com>
2025-08-23 21:48:09 -0400

    completed node project first pass

--- ModernJS/list/index.js
+++ ModernJS/list/index.js
@@ -2,20 +2,22 @@
 const fs = require("fs");
 const util = require("util");
 const chalk = require("chalk");
+const path = require("path");
 
 // Method #2
 //const lstat = util.promisify(fs.lstat);
 
 // Method #3
 const { lstat } = fs.promises;
+const targetDir = process.argv[2] || process.cwd();
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
     return;
   }
   const statPromises = filenames.map(filename => {
-    return lstat(filename);
+    return lstat(path.join(targetDir, filename));
   });
   const allStats = await Promise.all(statPromises);
   for (let stats of allStats) {
