0e52dd91baaaed3e3d589846d15eb919343a083f
mwoolf87 <mwoolfdev@gmail.com>
2025-08-30 13:21:30 -0400

    created methods to generate a userId and get and delete a user by Id

--- ModernJS/ecomm/repositories/users.js
+++ ModernJS/ecomm/repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
@@ -19,15 +20,39 @@ class UsersRepository {
       })
     );
   }
-  async create(attrs) {}
+  async create(attrs) {
+    attrs.id = this.randomId();
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    // write the updated "records array back to this.filename"
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
 
-  const users = await repo.getAll();
-  console.log(users);
+  await repo.delete("a36969cd");
 };
-
 test();
 const repo = new UsersRepository("users.json");
