f26d4a5310ff6d7130fc100b9cfa0bf39e8759e4
mwoolf87 <mwoolfdev@gmail.com>
2025-08-30 19:17:31 -0400

    added cookie library to track user, and signup and signout paths

--- ModernJS/ecomm/index.js
+++ ModernJS/ecomm/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const usersRepo = require("./repositories/users");
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
+app.use(
+  cookieSession({
+    keys: ["ashfhksgfkuuysgaf"],
+  })
+);
+app.get("/signup", (req, res) => {
   res.send(`
     <div>
+    Your ID is: ${req.session.userId}
         <form method="POST">
             <input name="email" placeholder="email"/>
             <input name="password" placeholder="password"/>
@@ -16,10 +24,41 @@ app.get("/", (req, res) => {
     `);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+app.post("/signup", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+  if (password !== passwordConfirmation) {
+    return res.send("passwords must match");
+  }
+  const user = await usersRepo.create({ email, password });
+
+  req.session.userId = user.id;
   res.send("Account created!!!");
 });
+
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("You are logged out");
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email"/>
+            <input name="password" placeholder="password"/>
+            <input name="passwordConfirmation" 
+            <button>Sign In</button>
+        </form>
+    </div>
+    `);
+});
+
+app.post("/signin", async (req, res) => {});
 
 app.listen(3000, () => {
   console.log("listening");
--- ModernJS/ecomm/repositories/users.js
+++ ModernJS/ecomm/repositories/users.js
@@ -26,6 +26,8 @@ class UsersRepository {
     records.push(attrs);
 
     await this.writeAll(records);
+
+    return attrs;
   }
 
   async writeAll(records) {
