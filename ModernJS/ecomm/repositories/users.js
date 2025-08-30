d50390e60147559658adefb967a27bcac5dfdaef
mwoolf87 <mwoolfdev@gmail.com>
2025-08-29 21:00:24 -0400

    created users.js and users.json file. Added logic to parse and return user data

---
+++ ModernJS/ecomm/repositories/users.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Creating a repository requires a filename");
+    }
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+  async getAll() {
+    // Open the file called this.filename
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf8",
+      })
+    );
+  }
+  async create(attrs) {}
+}
+
+const test = async () => {
+  const repo = new UsersRepository("users.json");
+
+  const users = await repo.getAll();
+  console.log(users);
+};
+
+test();
+const repo = new UsersRepository("users.json");
