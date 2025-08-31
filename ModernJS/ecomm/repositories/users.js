b87e40511f71934d8468b1866f5cd2e92c17256c
mwoolf87 <mwoolfdev@gmail.com>
2025-08-31 16:09:03 -0400

    test

--- ModernJS/ecomm/repositories/users.js
+++ ModernJS/ecomm/repositories/users.js
@@ -1,39 +1,41 @@
-const fs = require("fs");
-const crypto = require("crypto");
-const util = require("util");
+const fs = require('fs');
+const crypto = require('crypto');
+const util = require('util');
 
 const scrypt = util.promisify(crypto.scrypt);
+
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
-      throw new Error("Creating a repository requires a filename");
+      throw new Error('Creating a repository requires a filename');
     }
+
     this.filename = filename;
     try {
       fs.accessSync(this.filename);
     } catch (err) {
-      fs.writeFileSync(this.filename, "[]");
+      fs.writeFileSync(this.filename, '[]');
     }
   }
+
   async getAll() {
-    // Open the file called this.filename
     return JSON.parse(
       await fs.promises.readFile(this.filename, {
-        encoding: "utf8",
+        encoding: 'utf8'
       })
     );
   }
+
   async create(attrs) {
     attrs.id = this.randomId();
 
-    const salt = crypto.randomBytes(8).toString("hex");
-
+    const salt = crypto.randomBytes(8).toString('hex');
     const buf = await scrypt(attrs.password, salt, 64);
 
     const records = await this.getAll();
     const record = {
       ...attrs,
-      password: `${buf.toString("hex")}.${salt}}`,
+      password: `${buf.toString('hex')}.${salt}`
     };
     records.push(record);
 
@@ -42,25 +44,37 @@ class UsersRepository {
     return record;
   }
 
+  async comparePasswords(saved, supplied) {
+    // Saved -> password saved in our database. 'hashed.salt'
+    // Supplied -> password given to us by a user trying sign in
+    const [hashed, salt] = saved.split('.');
+    const hashedSupplied = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSupplied;
+  }
+
   async writeAll(records) {
-    // write the updated "records array back to this.filename"
     await fs.promises.writeFile(
       this.filename,
       JSON.stringify(records, null, 2)
     );
   }
+
   randomId() {
-    return crypto.randomBytes(4).toString("hex");
+    return crypto.randomBytes(4).toString('hex');
   }
+
   async getOne(id) {
     const records = await this.getAll();
     return records.find(record => record.id === id);
   }
+
   async delete(id) {
     const records = await this.getAll();
     const filteredRecords = records.filter(record => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
   async update(id, attrs) {
     const records = await this.getAll();
     const record = records.find(record => record.id === id);
@@ -68,19 +82,23 @@ class UsersRepository {
     if (!record) {
       throw new Error(`Record with id ${id} not found`);
     }
+
     Object.assign(record, attrs);
     await this.writeAll(records);
   }
+
   async getOneBy(filters) {
     const records = await this.getAll();
 
     for (let record of records) {
       let found = true;
+
       for (let key in filters) {
         if (record[key] !== filters[key]) {
           found = false;
         }
       }
+
       if (found) {
         return record;
       }
@@ -88,4 +106,4 @@ class UsersRepository {
   }
 }
 
-module.exports = new UsersRepository("users.json");
+module.exports = new UsersRepository('users.json');
