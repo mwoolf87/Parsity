ec5f404c3b11e4c1646ba5e52279e5d86c521ae4
mwoolf87 <mwoolfdev@gmail.com>
2025-08-31 16:16:49 -0400

    testing

--- ModernJS/ecomm/repositories/users.js
+++ ModernJS/ecomm/repositories/users.js
@@ -48,9 +48,9 @@ class UsersRepository {
     // Saved -> password saved in our database. 'hashed.salt'
     // Supplied -> password given to us by a user trying sign in
     const [hashed, salt] = saved.split('.');
-    const hashedSupplied = await scrypt(supplied, salt, 64);
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
 
-    return hashed === hashedSupplied;
+    return hashed === hashedSuppliedBuf.toString('hex');
   }
 
   async writeAll(records) {
