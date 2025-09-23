e0a953c335a60d0a4f307f600f58bf7628183923
mwoolf87 <mwoolfdev@gmail.com>
2025-09-22 22:00:47 -0400

    created conditionals DIR and practice if and else if statements

---
+++ WebDevBootcamp/Javascript/conditionals/app.js
@@ -0,0 +1,30 @@
+//  ---- IF STATEMENT
+// console.log("BEFORE CONDITIONAL");
+// if (1 + 2 === 2) {
+//   console.log("MATH STILL WORKS");
+// }
+// console.log("AFTER CONDITIONAL");
+
+// ----- ELSE IF STATEMENT
+
+// const dayOfWeek = "Monday";
+
+// if (dayOfWeek === "Monday") {
+//   console.log("UGGGHHH I HATE MONDAYS!!");
+// } else if (dayOfWeek === "Saturday") {
+//   console.log("YES! I LOVE SATURDAYS!!!");
+// }
+
+// 0-5 - FREE
+// 5 - 10 CHILD $10
+// 10-65 ADULT $20
+// 65+ SENIOR $10
+
+const age = 8;
+if (age < 5) {
+  console.log("you are a baby you get in for free");
+} else if (age < 10) {
+  console.log("you are a child, you pay $10");
+} else if (age < 65) {
+  console.log("you are an adult. you pay $20");
+}
