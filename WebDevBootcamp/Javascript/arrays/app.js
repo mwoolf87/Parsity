23630cae7d0304851cb68b3ffb01f40f1bd71b0f
mwoolf87 <mwoolfdev@gmail.com>
2025-09-23 22:51:43 -0400

    practice with array methods

--- WebDevBootcamp/Javascript/arrays/app.js
+++ WebDevBootcamp/Javascript/arrays/app.js
@@ -1,1 +1,27 @@
+const { arrayBuffer } = require("stream/consumers");
+
 // an array is a list of varying data types.
+const daysOfWeek = ["Monday", "Tuesday", "Wednesday"];
+daysOfWeek[0]; // Monday
+daysOfWeek[3]; // undefined
+
+// ---- ARRAY Methods
+//push - add to end
+// pop - remove from end
+// shift - remove from start
+// unshift - add to start
+let movieLine = [];
+movieLine.push("Ted, Corey, Lambo, Sean");
+console.log(movieLine); //['Ted, Corey, Lambo, Sean']
+movieLine.pop();
+console.log(movieLine); //['Ted, Corey, Lambo]
+
+// Additional Methods
+// concat - merge arrays
+// includes - look for value
+// indexOf - just like string.indexOf
+// join - creates a string from an array
+// reverse -  reverses an array
+// slice - copies a portion on an array
+// splice - removes/replaces elements
+// sort - sorts an array
