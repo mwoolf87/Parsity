7f9295b5c2f2e577e977f364b81ed424af040c50
mwoolf87 <mwoolfdev@gmail.com>
2025-10-04 16:45:36 -0400

    practicing creating, adding, and removing elements with javascript

--- WebDevBootcamp/DOM/app.js
+++ WebDevBootcamp/DOM/app.js
@@ -42,10 +42,42 @@ const image2 = document.querySelector("img");
 image2.style.width = "150px";
 image2.style.borderRadius = "50%";
 
+// in the below example I am looping over all span tags and setting their colors to the values of the colors array
+
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 const allSpans = document.querySelectorAll("span");
 for (let i = 0; i < allSpans.length; i++) {
   allSpans[i].style.color = colors[i];
 }
 
-// in the above example I am looping over all span tags and setting their colors to the values of the colors array
+// in the below example I am looping over all li tags and toggling on/off a class of "highlight based off its current value"
+
+const allLis = document.querySelectorAll("li");
+for (let li of allLis) {
+  li.classList.toggle("highlight");
+}
+const p = document.querySelector("p");
+p.append("I am new text YESSSSS");
+const newB = document.createElement("b");
+newB.append("Hi!");
+p.prepend(newB);
+
+// an example of adding a sibling to the h1 element "Silkie Chickens"
+const h2 = document.createElement("h2");
+h2.append("are adorable chickens");
+const h1 = document.querySelector("h1");
+h1.insertAdjacentElement("afterend", h2);
+const h3 = document.createElement("h3");
+h3.innerText = "I am an H3 Element";
+h1.after(h3);
+
+//the below examples slecs a div element. loops 100 times and creates a button i times/applies inner text i times and appends the buttons to the div element
+const div = document.querySelector("div");
+for (let i = 0; i < 100; i++) {
+  const button = document.createElement("button");
+  button.innerText = "I'm a button";
+  div.append(button);
+}
+//selects all buttons and removes them from the div element
+allbuttons = document.querySelectorAll("button");
+div.remove(allbuttons);
