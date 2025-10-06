1002ae774d2e36cc09399d62f80ae71338f30d9f
mwoolf87 <mwoolfdev@gmail.com>
2025-10-05 21:31:03 -0400

    started scoreKeeper project

--- WebDevBootcamp/DOM_Events/FormEvents/app.js
+++ WebDevBootcamp/DOM_Events/FormEvents/app.js
@@ -10,3 +10,27 @@ form.addEventListener("submit", function (e) {
   list.append(newLI);
   input.value = "";
 });
+
+// const product = document.querySelector("#product");
+// const qty = document.querySelector("#qty");
+
+// form.addEventListener("submit", e => {
+//   e.preventDefault();
+//   let productVal = product.value;
+//   let qtyVal = qty.value;
+//   const newLI = document.createElement("li");
+//   newLI.innerText = `${qtyVal} ${productVal}`;
+//   list.append(newLI);
+//   product.value = "";
+//   qty.value = "";
+// });
+
+// const h1 = document.querySelector("h1");
+// const input = document.querySelector("#username");
+
+// input.addEventListener("input", () => {
+//   h1.innerText = `Welcome, ${input.value}`;
+//   if (input.value === "") {
+//     h1.innerText = "Enter Your Username";
+//   }
+// });
