3c4d4bbe0e91d8967615ed30b2811afe7e98bbe6
mwoolf87 <mwoolfdev@gmail.com>
2025-10-09 23:46:41 -0400

    created AJAX DIR and started section on APIs and JSON

---
+++ WebDevBootcamp/AJAX/app.js
@@ -0,0 +1,20 @@
+// ------- AJAX
+// stands for Asynchronous Javascript And XML
+// ----- AJAJ later, JSON replaced XML
+// JSON stands for Java Script Oject Notation
+
+// example of JSON
+// {
+//     "squadName": "Super Hero guys",
+//     "hometown": "Flavortown",
+//     "formed": 2025,
+//     "members": [
+//         "Shredward",
+//         "Hawk",
+//         "Lambo",
+//     ],
+// }
+
+//JSON.parse(data)  takes JSON and turns it into an object which allows developers to work with and manupulate data
+
+//JSON.stringify(data) useful if we are trying to send data to an API
