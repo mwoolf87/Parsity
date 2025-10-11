18db11f182ea115d639033b348cffcb93186a6e4
mwoolf87 <mwoolfdev@gmail.com>
2025-10-10 22:21:12 -0400

    making fetch API requests with and without async await

--- WebDevBootcamp/AJAX/app.js
+++ WebDevBootcamp/AJAX/app.js
@@ -18,3 +18,36 @@
 //JSON.parse(data)  takes JSON and turns it into an object which allows developers to work with and manupulate data
 
 //JSON.stringify(data) useful if we are trying to send data to an API
+
+// GET REQUEST - get data
+// POST REQUEST - send data
+// PUT REQUEST - update data
+// DELETE REQUEST - delete data
+
+fetch("https://swapi.info/api/people/1")
+  .then(res => {
+    console.log("RESOLVED", res);
+    return res.json();
+  })
+  .then(data => {
+    console.log("JSON DONE", data);
+    return fetch("https://swapi.info/api/people/2");
+  })
+  .then(res => {
+    console.log(res);
+    return res.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(e => {
+    console.log("ERROR", e);
+  });
+
+const loadStarWarsPeople = async () => {
+  const res = await fetch("https://swapi.info/api/people/1");
+  const data = await res.json();
+  console.log(data);
+};
+
+loadStarWarsPeople();
