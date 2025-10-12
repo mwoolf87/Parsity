f6e8d67c7c6e255b7659b0d47f7a524e141d9088
mwoolf87 <mwoolfdev@gmail.com>
2025-10-11 22:50:32 -0400

    practice with axios

--- WebDevBootcamp/AJAX/app.js
+++ WebDevBootcamp/AJAX/app.js
@@ -24,30 +24,85 @@
 // PUT REQUEST - update data
 // DELETE REQUEST - delete data
 
-fetch("https://swapi.info/api/people/1")
-  .then(res => {
-    console.log("RESOLVED", res);
-    return res.json();
-  })
-  .then(data => {
-    console.log("JSON DONE", data);
-    return fetch("https://swapi.info/api/people/2");
-  })
-  .then(res => {
-    console.log(res);
-    return res.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(e => {
-    console.log("ERROR", e);
-  });
+// --------- FETCH API
 
-const loadStarWarsPeople = async () => {
-  const res = await fetch("https://swapi.info/api/people/1");
-  const data = await res.json();
-  console.log(data);
+// fetch("https://swapi.info/api/people/1")
+//   .then(res => {
+//     console.log("RESOLVED", res);
+//     return res.json();
+//   })
+//   .then(data => {
+//     console.log("JSON DONE", data);
+//     return fetch("https://swapi.info/api/people/2");
+//   })
+//   .then(res => {
+//     console.log(res);
+//     return res.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(e => {
+//     console.log("ERROR", e);
+//   });
+
+// const loadStarWarsPeople = async () => {
+//   try {
+//     const res = await fetch("https://swapi.info/api/people/1");
+//     const data = await res.json();
+//     console.log(data);
+//     const res2 = await fetch("https://swapi.info/api/people/2");
+//     const data2 = await res2.json();
+//     console.log(data2);
+//   } catch (e) {
+//     console.log("ERROR", e);
+//   }
+// };
+
+// loadStarWarsPeople();
+
+// ------- AXIOS
+
+// axios
+//   .get("https://swapi.info/api/people/1")
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(e => {
+//     console.log("ERROR!", e);
+//   });
+
+// const getStarWaresPerson = async id => {
+//   try {
+//     const res = await axios.get(`https://swapi.info/api/people/${id}`);
+//     console.log(res.data);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// getStarWaresPerson(1);
+// getStarWaresPerson(10);
+// getStarWaresPerson(15);
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  const newLI = document.createElement("li");
+  newLI.append(jokeText);
+  jokes.append(newLI);
 };
 
-loadStarWarsPeople();
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com", config);
+    return res.data.joke;
+  } catch (e) {
+    return "No Jokes Available! Sorry!";
+  }
+};
+button.addEventListener("click", addNewJoke);
+getDadJoke();
