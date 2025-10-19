c95ba6cb301873b5794f4095fe56357c4ed4cc6c
mwoolf87 <mwoolfdev@gmail.com>
2025-10-18 22:26:36 -0400

    set up deleteBookById and createBook, rendered books to BookList and toggle rendered BookEdit component

--- ModernReactWithRedux/books/src/App.js
+++ ModernReactWithRedux/books/src/App.js
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
-import React from "react";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = id => {
+    const updatedBooks = books.filter(book => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
   const createBook = title => {
-    const updatedBooks = [...books, title];
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
     setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- ModernReactWithRedux/books/src/components/BookShow.js
+++ ModernReactWithRedux/books/src/components/BookShow.js
@@ -1,7 +1,33 @@
-import React from "react";
+import BookEdit from "./BookEdit";
+import { useState } from "react";
 
-const BookShow = () => {
-  return <div>BookShow</div>;
+const BookShow = ({ book, onDelete }) => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit />;
+  }
+  return (
+    <div className="book-show">
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default BookShow;
