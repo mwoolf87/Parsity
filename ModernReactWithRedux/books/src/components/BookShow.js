eba402fc4751740f17c76f14d88d8e79447398a6
mwoolf87 <mwoolfdev@gmail.com>
2025-10-19 19:04:46 -0400

    refactored BookEdit component by eliminating onEdit prop and passing id and title into onSubmit which is already in BookEdit

--- ModernReactWithRedux/books/src/components/BookShow.js
+++ ModernReactWithRedux/books/src/components/BookShow.js
@@ -11,12 +11,18 @@ const BookShow = ({ book, onDelete, onEdit }) => {
     setShowEdit(!showEdit);
   };
 
+  const handleSubmit = (id, newTitle) => {
+    setShowEdit(false);
+    onEdit(id, newTitle);
+  };
+
   let content = <h3>{book.title}</h3>;
   if (showEdit) {
-    content = <BookEdit book={book} onEdit={onEdit} />;
+    content = <BookEdit book={book} onSubmit={handleSubmit} />;
   }
   return (
     <div className="book-show">
+      <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="books" />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
