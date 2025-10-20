a46b6d79d54158e23aa1d8f8eacbb9c0b7508169
mwoolf87 <mwoolfdev@gmail.com>
2025-10-19 23:02:27 -0400

    practice with context

--- ModernReactWithRedux/books/src/components/BookList.js
+++ ModernReactWithRedux/books/src/components/BookList.js
@@ -1,13 +1,21 @@
-import React from "react";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 const BookList = ({ books, onDelete, onEdit }) => {
+  const { count, incrementCount } = useContext(BooksContext);
   const renderedBooks = books.map(book => {
     return (
       <BookShow onEdit={onEdit} onDelete={onDelete} book={book} key={book.id} />
     );
   });
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {count}
+      <button onClick={incrementCount}>Click</button>
+      {renderedBooks}
+    </div>
+  );
 };
 
 export default BookList;
