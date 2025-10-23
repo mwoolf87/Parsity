c2ec1ce4eed06c7d2645ab97c63d9d8c78c1be62
mwoolf87 <mwoolfdev@gmail.com>
2025-10-22 23:17:21 -0400

    nuked and rebuilt app due to enviornment issues

--- ModernReactWithRedux/comps/src/App.js
+++ ModernReactWithRedux/comps/src/App.js
@@ -1,34 +1,5 @@
-import React from "react";
-import Button from "./Button";
-
-const App = () => {
-  return (
-    <div>
-      <div>
-        <Button success rounded outline>
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          Buy NOW!!
-        </Button>
-      </div>
-      <div>
-        <Button warning>See Deal!</Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads
-        </Button>
-      </div>
-      <div>
-        <Button secondary rounded>
-          Do Something
-        </Button>
-      </div>
-    </div>
-  );
-};
+function App() {
+  return <div>Hello</div>;
+}
 
 export default App;
--- ModernReactWithRedux/comps/src/index.js
+++ ModernReactWithRedux/comps/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import reactDOM from "react-dom/client";
-import App from "./App";
+import "./index.css";
+import App from "./App.js";
 
 const el = document.getElementById("root");
 const root = reactDOM.createRoot(el);
