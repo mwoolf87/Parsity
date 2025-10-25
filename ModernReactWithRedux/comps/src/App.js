4f5260c77ad04d467733eb65905ec42df86a9edf
mwoolf87 <mwoolfdev@gmail.com>
2025-10-24 21:03:08 -0400

    installed react-icons added margin to icons in buttons

--- ModernReactWithRedux/comps/src/App.js
+++ ModernReactWithRedux/comps/src/App.js
@@ -1,19 +1,25 @@
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 import Button from "./Button";
 function App() {
   return (
     <div>
       <div>
         <Button success rounded outline>
+          <GoBell />
           Click me!!
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoCloudDownload />
           Buy Now!
         </Button>
       </div>
       <div>
-        <Button warning>See Deal!</Button>
+        <Button warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
       </div>
       <div>
         <Button secondary outline>
