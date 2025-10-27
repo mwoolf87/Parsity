cfcb27a41b0ec4f46d83908e2324ddc628db7b13
mwoolf87 <mwoolfdev@gmail.com>
2025-10-26 21:52:09 -0400

    styled modal wrote logic to display modal on ModalPage

--- ModernReactWithRedux/comps/src/pages/ModalPage.js
+++ ModernReactWithRedux/comps/src/pages/ModalPage.js
@@ -8,12 +8,30 @@ function ModalPage() {
   const handleClick = () => {
     setShowModal(true);
   };
-  return (
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
     <div>
+      <Button onClick={handleClose} primary>
+        I Accept
+      </Button>
+    </div>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
+  return (
+    <div className="relative">
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal />}
+      {showModal && modal}
     </div>
   );
 }
