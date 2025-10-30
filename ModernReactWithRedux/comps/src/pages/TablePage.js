dbc8ed2f2122e480b7eab9a539fbf2d26e02eb46
mwoolf87 <mwoolfdev@gmail.com>
2025-10-29 22:20:21 -0400

    created SortableTable component and created a dynamic header function

--- ModernReactWithRedux/comps/src/pages/TablePage.js
+++ ModernReactWithRedux/comps/src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -14,15 +15,20 @@ function TablePage() {
       label: "Color",
       render: fruit => <div className={`p-3 m-3 ${fruit.color}`}></div>,
     },
-    { label: "Score", render: fruit => fruit.score },
+    {
+      label: "Score",
+      render: fruit => fruit.score,
+      header: () => <th>Score</th>,
+    },
   ];
 
   const keyFn = fruit => {
     return fruit.name;
   };
+
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
